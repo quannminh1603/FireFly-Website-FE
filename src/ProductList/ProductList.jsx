00de120d72085ed32db4705ee4217555bcc78fb1
kiet <kietlt.com>
2023-10-14 00:09:17 +0700

    code

--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -1,38 +1,47 @@
 import React from 'react';
 import './ProductList.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fashoppingcart } from '@fortawesome/free-solid-svg-icons'
 const ProductList = () => {
     const products = [
         {
             id: 1,
-            name: 'Product 1',
-            image: 'img/sanphammoi.jpg',
+            name: 'Jordan delta 2 ',
+            image: 'img/prices.jpg',
+            price:'4.000.000 VNĐ'
         },
         {
             id: 2,
-            name: 'Product 2',
-            image: 'img/sanphammoi1.jpg',
+            name: ' Jordan delta 2',
+            image: 'img/prices.jpg',
+            price:'4.000.000 VNĐ'
         },
         {
             id: 3,
-            name: 'Product 3',
-            image: 'img/sanphammoi2.jpg',
+            name: 'Jordan delta 2',
+            image: 'img/prices.jpg',
+            price:'4.000.000 VNĐ'
         },
     ];
 
     return (
         <div className='Productlist'>
-           
             <h1>Sản Phẩm Mới</h1>
-            
+            <hr className='line' />
             <div className="product-list">
-               
+
                 {products.map((product) => (
                     <div key={product.id} className="product-item">
-                        <img src={process.env.PUBLIC_URL + '/' + product.image} alt={product.name} />
-                       
-                            <h3>{product.name}</h3>
-                       
+                        <div className="product-image">
+                            <img src={process.env.PUBLIC_URL + '/' + product.image} alt={product.name }   />
+                          
+                            <div className="cart-icon">
+                                <i className="fa-solid fa-cart-shopping" aria-hidden="true" />
+
+                            </div>
+                        </div>
+                        <h3>{product.name}</h3>
+                        <h3>{product.price}</h3>
                     </div>
                 ))}
             </div>
